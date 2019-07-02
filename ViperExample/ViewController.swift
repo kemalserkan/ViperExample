4b8e46640bf485289568e2487f5e461e1b7c35b5
Kemal YILDIRIM <kemalserkanyildirim@kemal-macbook-pro.local>
2019-07-02 12:11:49 +0300

    Initial Commit

---
+++ ViperExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperExample
+//
+//  Created by Kemal Serkan YILDIRIM on 2.07.2019.
+//  Copyright © 2019 Kemal Serkan YILDIRIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ViperExampleTests/ViperExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  ViperExampleTests.swift
+//  ViperExampleTests
+//
+//  Created by Kemal Serkan YILDIRIM on 2.07.2019.
+//  Copyright © 2019 Kemal Serkan YILDIRIM. All rights reserved.
+//
+
+import XCTest
+@testable import ViperExample
+
+class ViperExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
